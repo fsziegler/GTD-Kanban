9d014f7071a48367bc9d4619da1b12493c3bbab7
fred <fsziegler@gmail.com>
2016-02-05 12:21:11 -0800

    Implemented static elements of Kanban window background.

--- qt/qt_test001/kanbanwidget.cpp
+++ qt/qt_test001/kanbanwidget.cpp
@@ -1,10 +1,15 @@
 #include "kanbanwidget.h"
 #include <QPainter>
 #include <QPaintEvent>
+#include <QFontMetrics>
+#include <QTreeWidget>
 
 KanbanWidget::KanbanWidget(QWidget *parent) : QWidget(parent)
 {
-
+   setAutoFillBackground(true);
+   QPalette Pal(palette());
+   Pal.setColor(QPalette::Background, QColor(255, 255, 128));
+   setPalette(Pal);
 }
 
 void KanbanWidget::paintEvent(QPaintEvent *pntEvent)
@@ -14,8 +19,48 @@ void KanbanWidget::paintEvent(QPaintEvent *pntEvent)
 
    QPainter painter(this);
    painter.save();
-   painter.setPen(Qt::blue);
-   painter.setFont(QFont("Arial", 24));
-   painter.drawText(rect, Qt::AlignCenter, "(Kanban Graph Goes here)");
+   painter.setPen(Qt::lightGray);
+   painter.setFont(QFont("Arial", 48));
+   painter.drawText(rect, Qt::AlignCenter, "Kanban Graph");
+
+   painter.setPen(QColor(0, 49, 83));
+   painter.setFont(QFont("Arial", 24, QFont::DemiBold));
+
+   QFontMetrics fMetrics(painter.font());
+   int height = fMetrics.height() * 1.5;
+   QPoint lhsColHrd(rect.left(), height);
+   QPoint rhsColHrd(rect.right(), height);
+
+   QRect readyRect(rect.left(), rect.top(), rect.right()/3, height);
+   static const QBrush b(Qt::white);
+   painter.setBackground(b);
+   painter.fillRect(QRect(QPoint(0,0), rhsColHrd), b);
+
+   // Draw outline rect & section dividers
+   QBrush lineBrush(Qt::darkGray);
+   painter.setPen(QPen(lineBrush, 6, Qt::SolidLine, Qt::SquareCap));
+   painter.drawRect(rect);
+   painter.setPen(QPen(lineBrush, 4, Qt::DashDotLine, Qt::RoundCap));
+   QPoint top1_3(rect.right()/3, rect.top());
+   QPoint bottom1_3(rect.right()/3, rect.bottom());
+   QPoint top2_3(2*rect.right()/3, rect.top());
+   QPoint bottom2_3(2*rect.right()/3, rect.bottom());
+   painter.drawLine(top1_3, bottom1_3);
+   painter.drawLine(top2_3, bottom2_3);
+
+   // Draw Kanban header line
+   painter.setPen(QPen(lineBrush, 3, Qt::SolidLine, Qt::SquareCap));
+   painter.drawLine(lhsColHrd, rhsColHrd);
+
+   // Draw Kanban header text
+   painter.setPen(QColor(0, 49, 83));
+   painter.drawText(readyRect, Qt::AlignCenter, "Ready");
+   readyRect.setLeft(readyRect.right());
+   readyRect.setRight(2*readyRect.left());
+   painter.drawText(readyRect, Qt::AlignCenter, "Doing");
+   readyRect.setLeft(readyRect.right());
+   readyRect.setRight(rect.right());
+   painter.drawText(readyRect, Qt::AlignCenter, "Done!");
+
    painter.restore();
 }
